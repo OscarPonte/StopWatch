901ac6cde5190cf99ef9e00b058d47aaa8e6cb7d
Oscar <Oscarponteuk@gmail.com>
2020-05-20 18:53:20 +0100

    Adding Projects to GitHub

---
+++ StopWatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var stopWatch = new StopWatch();
+
+                Console.Write("Press Enter to START the StopWatch!");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                    break;
+
+                Console.WriteLine(stopWatch.Start());
+
+
+                Console.Write("Press Enter to STOP the StopWatch!!");
+                var input1 = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input1))
+                    break;
+
+                Console.WriteLine(stopWatch.Stop());
+                Console.WriteLine(stopWatch.Duration() + " Seconds");
+
+                Console.Write("To Try one more time press ENTER");
+                var input2 = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input2))
+                    break;
+            }
+        }
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime _start;
+        private DateTime _stop;
+        private TimeSpan _duration;
+
+        public string Start()
+        {
+            _start = DateTime.Now;
+            return _start.ToLongTimeString();
+        }
+
+        public string Stop()
+        {
+            _stop = DateTime.Now;
+            return _stop.ToLongTimeString();
+        }
+
+        public string Duration()
+        {
+            _duration = _stop - _start;
+            return _duration.TotalSeconds.ToString("N");
+        }
+    }
+}
